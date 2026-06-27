c167feaec82e06f70f9704ea529ec23a96c02916
FunBreakSEO <doganizzetcan2@gmail.com>
2026-06-27 14:19:52 +0300

    fix: CompetitorModule PrismaService eksikliği giderildi

--- apps/api/src/modules/competitor/competitor.module.ts
+++ apps/api/src/modules/competitor/competitor.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CompetitorService } from './competitor.service';
 import { CompetitorController } from './competitor.controller';
 import { DataForSeoModule } from '../dataforseo/dataforseo.module';
+import { PrismaService } from '../../prisma.service';
 
 @Module({
   imports: [DataForSeoModule],
   controllers: [CompetitorController],
-  providers: [CompetitorService],
+  providers: [CompetitorService, PrismaService],
   exports: [CompetitorService],
 })
 export class CompetitorModule {}
